d5f620de096121cd884607734fbb592a828cd255
uitricks <learn.uitricks@gmail.com>
2022-09-17 19:51:55 +0530

    Car Class with Public Property

---
+++ OOPS/OOPS/1_Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.OOPS
+{
+    public class Car
+    {
+       public string Color = "red";
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOPS.OOPS;
+using System;
+
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car redCar = new Car();
+            Console.WriteLine($"car color is {redCar.Color}");
+        }
+    }
+}
