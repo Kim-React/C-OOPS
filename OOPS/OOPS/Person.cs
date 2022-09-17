37ec86dd9b66ab533addfd1f6e9c9a0050453f1a
uitricks <learn.uitricks@gmail.com>
2022-09-17 20:14:32 +0530

    4AcessModifiers

---
+++ OOPS/OOPS/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS.OOPS
+{
+    class Person
+    {
+        private string name; // field
+
+        public Person(string _name)
+        {
+            this.name = _name;
+        }
+        public string Name   // property
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+    }
+
+}
--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -7,11 +7,14 @@ namespace OOPS
     {
         static void Main(string[] args)
         {
-            Car car1 = new Car("Mustang"); ;
-            Console.WriteLine(car1.model);
+            //Car car1 = new Car("Mustang"); ;
+            //Console.WriteLine(car1.model);
 
-            Car car2 = new Car("Ford"); ;
-            Console.WriteLine(car2.model);
+            //Car car2 = new Car("Ford"); ;
+            //Console.WriteLine(car2.model);
+
+            Person person1 = new Person("Kumar");
+            Console.WriteLine(person1.Name)
 
         }
     }
